f472e07ab451f4468c341473d22d7dd7b8b4da92
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-03-06 18:50:19 -0700

    added firebase

---
+++ assets/javascript/ontime.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBotbBY-lxslbb-cQaeMtvpFeZuRQNYyvA",
+	authDomain: "ontime-7e71b.firebaseapp.com",
+	databaseURL: "https://ontime-7e71b.firebaseio.com",
+	projectId: "ontime-7e71b",
+	storageBucket: "",
+	messagingSenderId: "680626336941"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
