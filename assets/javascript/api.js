c07ae0aeff493a0374d89050173af8341dd7e193
ben <bwilly91@msn.com>
2018-03-10 12:50:28 -0700

    end of class SAT

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,32 +1,32 @@
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyBotbBY-lxslbb-cQaeMtvpFeZuRQNYyvA",
-	authDomain: "ontime-7e71b.firebaseapp.com",
-	databaseURL: "https://ontime-7e71b.firebaseio.com",
-	projectId: "ontime-7e71b",
-	storageBucket: "ontime-7e71b.appspot.com",
-	messagingSenderId: "680626336941"
-};
-firebase.initializeApp(config);
+// // Initialize Firebase
+// var config = {
+// 	apiKey: "AIzaSyBotbBY-lxslbb-cQaeMtvpFeZuRQNYyvA",
+// 	authDomain: "ontime-7e71b.firebaseapp.com",
+// 	databaseURL: "https://ontime-7e71b.firebaseio.com",
+// 	projectId: "ontime-7e71b",
+// 	storageBucket: "ontime-7e71b.appspot.com",
+// 	messagingSenderId: "680626336941"
+// };
+// firebase.initializeApp(config);
 
-var database = firebase.database();
+// var database = firebase.database();
 
 
-// FirebaseUI config.
-var uiConfig = {
-	signInSuccessUrl: 'index.html',
-	signInOptions: [
-		// Leave the lines as is for the providers you want to offer your users.
-		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-	],
-	// Terms of service url.
-	tosUrl: '<your-tos-url>'
-};
+// // FirebaseUI config.
+// var uiConfig = {
+// 	signInSuccessUrl: 'index.html',
+// 	signInOptions: [
+// 		// Leave the lines as is for the providers you want to offer your users.
+// 		firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+// 	],
+// 	// Terms of service url.
+// 	tosUrl: '<your-tos-url>'
+// };
 
-// Initialize the FirebaseUI Widget using Firebase.
-var ui = new firebaseui.auth.AuthUI(firebase.auth());
-// The start method will wait until the DOM is loaded.
-ui.start('#firebaseui-auth-container', uiConfig);
+// // Initialize the FirebaseUI Widget using Firebase.
+// var ui = new firebaseui.auth.AuthUI(firebase.auth());
+// // The start method will wait until the DOM is loaded.
+// ui.start('#firebaseui-auth-container', uiConfig);
 
 ///////----------------------------------------------------------------------------------------------------------------------------------
 // Google Maps API
@@ -51,8 +51,8 @@ function initMap() {
 
 	var trafficLayer = new google.maps.TrafficLayer();
 	trafficLayer.setMap(map);
-}
-var geocoder = new google.maps.Geocoder;
+	
+	var geocoder = new google.maps.Geocoder;
 
 
 //////
@@ -67,7 +67,7 @@ service.getDistanceMatrix(
 		destinations: [destinationA],
 		travelMode: 'DRIVING',
 		drivingOptions: {
-			departureTime: new Date(Date.now() + N), //leaveing now-ish
+			departureTime: new Date(Date.now()), //leaveing now-ish
 			trafficModel: 'pessimistic'
 		},
 		unitSystem: google.maps.UnitSystem.IMPERIAL,
@@ -75,26 +75,28 @@ service.getDistanceMatrix(
 		avoidTolls: false
 	}, callback);
 
-// blank callback function
-// function callback(response, status) {
-//	// See Parsing the Results for
-//	// the basics of a callback function.
-//}
-
+}
 function callback(response, status) {
 	if (status == 'OK') {
-		console.log(response);
+		// console.log(response);
 		var origins = response.originAddresses;
 		var destinations = response.destinationAddresses;
-
+		// console.log(response.originAddresses);
+		// console.log(response.destinationAddresses);
 		for (var i = 0; i < origins.length; i++) {
 			var results = response.rows[i].elements;
+			console.log(results);
 			for (var j = 0; j < results.length; j++) {
 				var element = results[j];
 				var distance = element.distance.text;
 				var duration = element.duration.text;
 				var from = origins[i];
 				var to = destinations[j];
+				console.log(element);
+				console.log(distance);
+				console.log(duration);
+				console.log(from);
+				console.log(to);
 			}
 		}
 	}
@@ -107,153 +109,153 @@ function callback(response, status) {
 // Google calendar API
 ///////----------------------------------------------------------------------------------------------------------------------------------
 
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '680626336941-jq4kcktr2ghv678kmlittf9th7sndsak.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyAlI91WupqEYc4XBk5dBrfcNKekA_e9aZ0';
+// // Client ID and API key from the Developer Console
+// var CLIENT_ID = '680626336941-jq4kcktr2ghv678kmlittf9th7sndsak.apps.googleusercontent.com';
+// var API_KEY = 'AIzaSyAlI91WupqEYc4XBk5dBrfcNKekA_e9aZ0';
 
-// Array of API discovery doc URLs for APIs used by the quickstart
-var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
+// // Array of API discovery doc URLs for APIs used by the quickstart
+// var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
+// // Authorization scopes required by the API; multiple scopes can be
+// // included, separated by spaces.
+// var SCOPES = "https://www.googleapis.com/auth/calendar.readonly";
 
-var authorizeButton = document.getElementById('firebaseui-auth-container');
-var signoutButton = document.getElementById('signOutBtn');
-
-/**
- *  On load, called to load the auth2 library and API client library.
- */
-function handleClientLoad() {
-	gapi.load('client:auth2', initClient);
-}
-
-/**
- *  Initializes the API client library and sets up sign-in state
- *  listeners.
- */
-function initClient() {
-	gapi.client.init({
-		apiKey: API_KEY,
-		clientId: CLIENT_ID,
-		discoveryDocs: DISCOVERY_DOCS,
-		scope: SCOPES
-	}).then(function () {
-			// Listen for sign-in state changes.
-			gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
-
-			// Handle the initial sign-in state.
-			updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
-			authorizeButton.onclick = handleAuthClick;
-			signoutButton.onclick = handleSignoutClick;
-		});//-was inbetween curly and semi
-}
-
-/**
- *  Called when the signed in status changes, to update the UI
- *  appropriately. After a sign-in, the API is called.
- */
-// function updateSigninStatus(isSignedIn) {
-// 	if (isSignedIn) {
-
-// from ontime.js -----------------------------
-firebase.auth().onAuthStateChanged(function (user) {
-	if (user) {
-		// -----------------------------------------
-		// authorizeButton.style.display = 'none';
-		// signoutButton.style.display = 'block';
-
-		// from ontime.js-------------------------------------------
-		var displayName = user.displayName;
-		var email = user.email;
-		var emailVerified = user.emailVerified;
-		var photoURL = user.photoURL;
-		var uid = user.uid;
-		var phoneNumber = user.phoneNumber;
-		var providerData = user.providerData;
-		user.getIdToken().then(function (accessToken) {
-			$("#firebaseui-auth-container").hide();
-			document.getElementById('account-details').innerHTML = '<img class="userImage img-circle" src="' + photoURL + '" alt="User Image">' + displayName;
-			$("#signOutBtn").on("click", function () {
-				firebase.auth().signOut().then(function () {
-					console.log('Signed Out');
-				}, function (error) {
-					console.error('Sign Out Error', error);
-				});
-			});
-		});
-
-		console.log("User is Signed IN!");
-		// --------------------------------------------------------------
-		listUpcomingEvents();
-	} else {
-		// authorizeButton.style.display = 'block';
-		// signoutButton.style.display = 'none';
-
-		// From ontime.js -------------------------------------------
-		// User is signed out.
-		document.getElementById('account-details').innerHTML = '';
-		document.getElementById('sign-in').innerHTML = '';
-		$("#firebaseui-auth-container").show();
-		console.log("User is signed out");
-		//-------------------------------------------------------------
-	}
-});
-
-/**
- *  Sign in the user upon button click.
-//  */
-function handleAuthClick(event) {
-	gapi.auth2.getAuthInstance().signIn();
-}
+// var authorizeButton = document.getElementById('firebaseui-auth-container');
+// var signoutButton = document.getElementById('signOutBtn');
 
 // /**
-//  *  Sign out the user upon button click.
+//  *  On load, called to load the auth2 library and API client library.
 //  */
-function handleSignoutClick(event) {
-	gapi.auth2.getAuthInstance().signOut();
-}
+// function handleClientLoad() {
+// 	gapi.load('client:auth2', initClient);
+// }
 
-/**
- * Append a pre element to the body containing the given message
- * as its text node. Used to display the results of the API call.
- *
- * @param {string} message Text to be placed in pre element.
- */
-function appendPre(message) {
-	var pre = document.getElementById('calendar');
-	var textContent = document.createTextNode(message + '\n');
-	pre.appendChild(textContent);
-}
+// /**
+//  *  Initializes the API client library and sets up sign-in state
+//  *  listeners.
+//  */
+// function initClient() {
+// 	gapi.client.init({
+// 		apiKey: API_KEY,
+// 		clientId: CLIENT_ID,
+// 		discoveryDocs: DISCOVERY_DOCS,
+// 		scope: SCOPES
+// 	}).then(function () {
+// 			// Listen for sign-in state changes.
+// 			gapi.auth2.getAuthInstance().isSignedIn.listen(updateSigninStatus);
 
-/**
- * Print the summary and start datetime/date of the next ten events in
- * the authorized user's calendar. If no events are found an
- * appropriate message is printed.
- */
-function listUpcomingEvents() {
-	gapi.client.calendar.events.list({
-		'calendarId': 'primary',
-		'timeMin': (new Date()).toISOString(),
-		'showDeleted': false,
-		'singleEvents': true,
-		'maxResults': 10,
-		'orderBy': 'startTime'
-	}).then(function (response) {
-		var events = response.result.items;
-		appendPre('Upcoming events:');
+// 			// Handle the initial sign-in state.
+// 			updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
+// 			authorizeButton.onclick = handleAuthClick;
+// 			signoutButton.onclick = handleSignoutClick;
+// 		});//-was inbetween curly and semi
+// }
 
-		if (events.length > 0) {
-			for (i = 0; i < events.length; i++) {
-				var event = events[i];
-				var when = event.start.dateTime;
-				if (!when) {
-					when = event.start.date;
-				}
-				appendPre(event.summary + ' (' + when + ')')
-			}
-		} else {
-			appendPre('No upcoming events found.');
-		}
-	});
-}
+// /**
+//  *  Called when the signed in status changes, to update the UI
+//  *  appropriately. After a sign-in, the API is called.
+//  */
+// // function updateSigninStatus(isSignedIn) {
+// // 	if (isSignedIn) {
+
+// // from ontime.js -----------------------------
+// firebase.auth().onAuthStateChanged(function (user) {
+// 	if (user) {
+// 		// -----------------------------------------
+// 		// authorizeButton.style.display = 'none';
+// 		// signoutButton.style.display = 'block';
+
+// 		// from ontime.js-------------------------------------------
+// 		var displayName = user.displayName;
+// 		var email = user.email;
+// 		var emailVerified = user.emailVerified;
+// 		var photoURL = user.photoURL;
+// 		var uid = user.uid;
+// 		var phoneNumber = user.phoneNumber;
+// 		var providerData = user.providerData;
+// 		user.getIdToken().then(function (accessToken) {
+// 			$("#firebaseui-auth-container").hide();
+// 			document.getElementById('account-details').innerHTML = '<img class="userImage img-circle" src="' + photoURL + '" alt="User Image">' + displayName;
+// 			$("#signOutBtn").on("click", function () {
+// 				firebase.auth().signOut().then(function () {
+// 					console.log('Signed Out');
+// 				}, function (error) {
+// 					console.error('Sign Out Error', error);
+// 				});
+// 			});
+// 		});
+
+// 		console.log("User is Signed IN!");
+// 		// --------------------------------------------------------------
+// 		listUpcomingEvents();
+// 	} else {
+// 		// authorizeButton.style.display = 'block';
+// 		// signoutButton.style.display = 'none';
+
+// 		// From ontime.js -------------------------------------------
+// 		// User is signed out.
+// 		document.getElementById('account-details').innerHTML = '';
+// 		document.getElementById('sign-in').innerHTML = '';
+// 		$("#firebaseui-auth-container").show();
+// 		console.log("User is signed out");
+// 		//-------------------------------------------------------------
+// 	}
+// });
+
+// /**
+//  *  Sign in the user upon button click.
+// //  */
+// function handleAuthClick(event) {
+// 	gapi.auth2.getAuthInstance().signIn();
+// }
+
+// // /**
+// //  *  Sign out the user upon button click.
+// //  */
+// function handleSignoutClick(event) {
+// 	gapi.auth2.getAuthInstance().signOut();
+// }
+
+// /**
+//  * Append a pre element to the body containing the given message
+//  * as its text node. Used to display the results of the API call.
+//  *
+//  * @param {string} message Text to be placed in pre element.
+//  */
+// function appendPre(message) {
+// 	var pre = document.getElementById('calendar');
+// 	var textContent = document.createTextNode(message + '\n');
+// 	pre.appendChild(textContent);
+// }
+
+// /**
+//  * Print the summary and start datetime/date of the next ten events in
+//  * the authorized user's calendar. If no events are found an
+//  * appropriate message is printed.
+//  */
+// function listUpcomingEvents() {
+// 	gapi.client.calendar.events.list({
+// 		'calendarId': 'primary',
+// 		'timeMin': (new Date()).toISOString(),
+// 		'showDeleted': false,
+// 		'singleEvents': true,
+// 		'maxResults': 10,
+// 		'orderBy': 'startTime'
+// 	}).then(function (response) {
+// 		var events = response.result.items;
+// 		appendPre('Upcoming events:');
+
+// 		if (events.length > 0) {
+// 			for (i = 0; i < events.length; i++) {
+// 				var event = events[i];
+// 				var when = event.start.dateTime;
+// 				if (!when) {
+// 					when = event.start.date;
+// 				}
+// 				appendPre(event.summary + ' (' + when + ')')
+// 			}
+// 		} else {
+// 			appendPre('No upcoming events found.');
+// 		}
+// 	});
+// }
