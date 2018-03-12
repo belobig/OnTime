c96b85a82af9c4b2c121fe8402bb06d85dad4618
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-03-12 14:26:07 -0600

    More stying fixes for signin and signout buttons

--- assets/javascript/ontime.js
+++ assets/javascript/ontime.js
@@ -1,7 +1,7 @@
 // $(document).ready(function () {
 
 //Initialize tooltips
-// $('[data-toggle="tooltip"]').tooltip();
+$('[data-toggle="tooltip"]').tooltip();
 
 // Initialize Firebase
 var config = {
